f6476d98e5ea63770986f82df5cd883b1d552279
Erwing Coss <erwingcoss21@hotmail.com>
2024-02-03 10:46:42 -0400

    Creating Interface and types.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,58 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+/**
+ * *Json body. and example of the data that is coming.
+ * 
+ * {
+    "id": 1,
+    "name": {
+      "english": "Bulbasaur",
+      "japanese": "フシギダネ",
+      "chinese": "妙蛙种子",
+      "french": "Bulbizarre"
+    },
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "base": {
+      "HP": 45,
+      "Attack": 49,
+      "Defense": 49,
+      "Sp. Attack": 65,
+      "Sp. Defense": 65,
+      "Speed": 45
+    }
+  }
+ */
+
+
+  type Name ={
+    english: string,
+    japanese: string,
+    chinese: string,
+    french: string
+  };
+
+  type Base = {
+    HP: number,
+    Attack: number,
+    Defense: number,
+    "Sp. Attack": number,
+    "Sp. Defense": number,
+    Speed: number
+  }
+
+
+  interface Pokemon{
+    id: number,
+    name: Name,
+    type: string[],
+    Base: Base
+  };
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
