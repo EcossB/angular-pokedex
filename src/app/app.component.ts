64fdeb76216fdfa78006a523c17327e6f9a2c2fc
Erwing Coss <erwingcoss21@hotmail.com>
2024-02-03 12:21:22 -0400

    adding a call method to retrieve pokemons data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 /**
  * *Json body. and example of the data that is coming.
@@ -48,19 +50,51 @@ import { RouterOutlet } from '@angular/router';
 
   interface Pokemon{
     id: number,
+    stid: string,
     name: Name,
     type: string[],
-    Base: Base
+    base: Base
   };
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Pokedex';
+
+  pokemons :Pokemon[] = [];
+
+  constructor(public http: HttpClient){}
+
+  callData(){
+    this.http.get('http://172.24.0.152/JSON/DATA/getjson.php?js=pokedex.json')
+    .subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.pokemons = response;
+
+        //cosa hecha por asarias.
+        for (let index = 0; index < 100; index++) {
+          let stid:string="";
+          stid=parseInt(response[index].id,10).toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping:false})
+          console.log(stid);
+         // let pokemons :Pokemon[] = [{id: response[index].id,stid: response[index].stid,name:response[index].name,type:response[index].type,base:response[index].base}];
+          this.pokemons.push({id: response[index].id,stid: stid,name:response[index].name,type:response[index].type,base:response[index].base});
+        }
+
+      },
+      error: (error) => {
+        console.log("Error de conexion ", error);
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.callData();
+  }
 }
