fef534f74d63a81fd1390638aa7cdb572c9e8288
Erwing Coss <erwingcoss21@hotmail.com>
2024-02-06 08:11:46 -0400

    Adding routes to the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DescriptionComponent } from './description/description.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'', title:'Pokedex', component:HomeComponent},
+    {path: 'descripcion', title:'Description', component: DescriptionComponent},
+    {path:'**', component: HomeComponent}
+];
