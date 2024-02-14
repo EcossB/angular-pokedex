0102b78a0fad8b18c341bfd11a3765dc7b53c90f
Erwing Coss <erwingcoss21@hotmail.com>
2024-02-14 13:57:08 -0400

    adding login and signup components to routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { DescriptionComponent } from './description/description.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 export const routes: Routes = [
     {path:'', title:'Pokedex', component:HomeComponent},
+    {path:'inicio', title:'Pokedex', component:HomeComponent},
     {path: 'descripcion', title:'Description', component: DescriptionComponent},
+    {path: 'login', title:'Iniciar Sesion', component: LoginComponent},
+    {path: 'signup', title:'Registrarse', component: SignupComponent},
     {path:'**', component: HomeComponent}
 ];
