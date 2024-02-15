864533d0f94aa13097646b1eb8803b860be359fa
Erwing Coss <erwingcoss21@hotmail.com>
2024-02-15 09:59:21 -0400

    login and signup adding formcontrolgroup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validator, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  
+    fb = inject(FormBuilder)
+
+    constructor(
+      http: HttpClient
+      ){}
+
+    form = this.fb.nonNullable.group({
+     username: ['', Validators.required],
+     password: ['', Validators.required]
+    });
+    
+    onSubmit(): void {
+      console.log('Submit');
+    }
+
     reloadPage() {
     window.location.reload();
     }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserRegister } from './userR.interface';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-reloadPage() {
-throw new Error('Method not implemented.');
-}
+
+  fb = inject(FormBuilder);
+
+  constructor(public http: HttpClient){}
+
+
+  formSignUp = this.fb.nonNullable.group({
+    name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    username: ['', Validators.required],
+    passwordd: ['', Validators.required]
+  })
+
+    onSubmit() {
+    this.http.post<{user:UserRegister}>('https://localhost:7033/register', this.formSignUp.getRawValue())
+    .subscribe( response => {
+      console.log('Response ',response);
+    })
+    //console.log(this.formSignUp.getRawValue());
+    }
+
+    reloadPage() {
+    window.location.reload();
+    }
 
 }
