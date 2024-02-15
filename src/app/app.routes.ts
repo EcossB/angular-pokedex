d3ae549341497d1560c24ca9832c8a59b87b5e51
Erwing Coss <erwingcoss21@hotmail.com>
2024-02-15 16:48:16 -0400

    adding can active to description component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { DescriptionComponent } from './description/description.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthGuardService } from './auth-guard.service';
 
 
 export const routes: Routes = [
     {path:'', title:'Pokedex', component:HomeComponent},
     {path:'inicio', title:'Pokedex', component:HomeComponent},
-    {path: 'descripcion', title:'Description', component: DescriptionComponent},
+    {path: 'descripcion', title:'Description', component: DescriptionComponent, canActivate: [AuthGuardService]},
     {path: 'login', title:'Iniciar Sesion', component: LoginComponent},
     {path: 'signup', title:'Registrarse', component: SignupComponent},
     {path:'**', component: HomeComponent}
